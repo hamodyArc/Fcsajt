75423aeaa0a892e022ddc8d2fb92de36d1102583
Hamody <yournewemail@example.com>
2025-01-27 21:02:17 +0100

    second update

---
+++ main.py
@@ -0,0 +1,23 @@
+from flask import Flask, render_template, request, redirect, url_for, session, flash
+import mysql.connector
+import bcrypt
+from datetime import timedelta
+import os
+
+app = Flask(__name__)
+app.secret_key = "Very_secret_secret_key"
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+@app.route("/login")
+def login():
+    return render_template('login.html')
+
+@app.route("/register")
+def register():
+    return render_template('register.html')
+
+if __name__ == '__main__':
+    app.run(debug=True)
