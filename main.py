3b13bf01cc2f76862d0fd4983a9069cb8e18bb7a
hamody <mohamad.alaktaa@elev.ga.ntig.se>
2025-02-10 19:44:50 +0100

    final

--- main.py
+++ main.py
@@ -6,8 +6,17 @@ import os
 app = Flask(__name__)
 app.secret_key = "Very_secret_secret_key"
 
-@app.route('/')
-def index():
+login_db = mysql.connector.connect(
+    host="localhost",
+    user="root",
+    password= "3232",
+    database="fcs"
+)
+
+cursor = login_db.cursor()
+
+@app.route('/home')
+def home():
     return render_template('index.html')
 
 @app.route("/login")
@@ -17,6 +26,50 @@ def login():
 @app.route("/register")
 def register():
     return render_template('register.html')
+
+@app.route("/registerform", methods=['POST'])
+def add_user():
+    username = request.form['username']
+    password = request.form['password']
+    email = request.form['email']
+
+    cursor.execute("SELECT * FROM users WHERE email = %s", (email,))
+    existing_user = cursor.fetchone()
+
+    if existing_user:
+        flash('Email already exists')
+        return redirect(url_for('register', messege='Email already exists'))
+    else:
+        cursor.execute("INSERT INTO users (username, password, email) VALUES (%s, %s, %s)", (username, password, email))
+        login_db.commit()
+        flash('You have successfully registered')
+        return redirect(url_for('login'))
+    
+@app.route("/login_form", methods=['POST'])
+def login_form():
+    username = request.form['username']
+    # email = request.form['email']
+    password = request.form['password']
+
+    cursor.execute("SELECT * FROM users WHERE username = %s", (username,))
+    user = cursor.fetchone()
+
+    if user and password == user[2]:
+        session['user'] = {'id': user[0], 'username': user[1], 'email': user[3]}
+        flash('Successfully logged in!', "success")
+        return redirect(url_for("home"))
+    else:
+        return render_template("login.html", message="Invalid credentials")
+
+
+@app.route('/logout')
+def logout():
+    session.pop('user', None)
+    flash('Successfully logged out!', "success")
+    return redirect(url_for('home'))
+
+
+
 
 if __name__ == '__main__':
     app.run(debug=True)
